48d663bee1d285fed22a37ee52fe4c460be062fa
Nitroxt5 <filippov.maxim52@gmail.com>
2022-12-15 19:39:55 +0300

    Profile & recognize update
    Images now pop up on click

--- FERApp/FERApp/settings.py
+++ FERApp/FERApp/settings.py
@@ -89,6 +89,12 @@ WSGI_APPLICATION = 'FERApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.environ['DB_NAME'],
+        # 'USER': os.environ['DB_USER'],
+        # 'PASSWORD': os.environ['DB_PASSWORD'],
+        # 'HOST': os.environ['DB_HOST'],
+        # 'PORT': os.environ['DB_PORT'],
+        # 'OPTIONS': {'sslmode': 'require'}
         'NAME': db,
         'USER': user,
         'PASSWORD': password,
